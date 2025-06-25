9f1c6a4ea54af7d9846767cad793d6b1dcf418f4
stewiegriffinaaa <3795447655@qq.com>
2025-06-25 11:28:17 +0800

    Create ConsumeControl.h

---
+++ ConsumeControl.h
@@ -0,0 +1,12 @@
+#include"ShopCarDate.h"
+using namespace std;
+void ConsumeDec(ShopCar example,ShopLinked example1,Goods type,int numb){
+  example.dec(type,numb);
+  example1.pop(type,numb);
+}
+void ConsumeAdd(ShopCar example,ShopLinked example1,Goods type,int numb){
+  example1.Insert(type,numb);
+  for(int i = 0;i<numb;i++){
+    example.add(type);
+  }
+}
