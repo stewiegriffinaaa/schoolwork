a69f724d2e34e7f1810349505eeab2b7e49a9ca7
stewiegriffinaaa <3795447655@qq.com>
2025-06-26 22:47:44 +0800

    Update Time.h

--- Time.h
+++ Time.h
@@ -3,11 +3,15 @@
 using namespace std;
 struct Time
 {
-  int year = 0;
-  int month = 0;
-  int day = 0;
+	int year = 0;
+	int month = 0;
+	int day = 0;
 };
-ostream& operator<<(ostream& out,Time& num){
-  out << num.year << "-" << num.month << "-" << num.day;
-  return out;
-} 
+ostream& operator<<(ostream& out, Time& num) {
+	out << num.year << "-" << num.month << "-" << num.day;
+	return out;
+}
+istream& operator>>(istream& in, Time& num) {
+	in >> num.year  >> num.month >> num.day;
+	return in;
+}//修改点七
