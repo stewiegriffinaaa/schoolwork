c46fd6ee145f93bf181f28f80f9fea3d36e0225a
stewiegriffinaaa <3795447655@qq.com>
2025-06-26 22:49:59 +0800

    Update ShopCar.h

--- ShopCar.h
+++ ShopCar.h
@@ -1,29 +1,31 @@
 #pragma once
 #include"Time.h"
 #include"Consume.h"
-#include"Goods.h"
+#include"goods.h"
 #include<vector>
-#include<sstream>
+#include<time.h>
 using namespace std;
-class ShopCar:public Consume
+#include<sstream>
+class ShopCar :public Consume
 {
 protected:
-  int ShopCarNum;
-  int ConsumeNum;
-  Time start;
-  Time end;
-  vector<Goods> num;
-  vector<Time> addnum;
+	int ShopCarNum;
+	int ConsumeNum;
+	Time start;
+	Time end;
+	vector<Goods> num;
+	vector<Time> addnum;
 public:
-  ShopCar(int _num,string _name,char _class,string _email,Time _Start,int _ShopCar,int _ConsumeNum,Time _start,Time _end);
-  ostringstream& show(ostringstream& out);
-  void add(Goods sample);
-  void dec(Goods type,int numb);
-  int getShopCarNum();
-  int getConsumeNum();
-  Time getStart();
-  Time getEnd();
-  vector<Goods> getNum();
-  vector<Time> getAddnum();
-  ~ShopCar();
+	ShopCar(string _num, string _name, char _class, string _email, Time _Start, int _ShopCar, int _ConsumeNum, Time _start, Time _end);
+	ostringstream& show(ostringstream& out);
+	void add(Goods sample);
+	void dec(Goods type, int numb);
+	int getShopCarNum();
+	int getConsumeNum();
+	Time getStart();
+	Time getEnd();
+	vector<Goods> getNum();
+	vector<Time> getAddnum();
+	void CompleteCut(Goods type);
+	~ShopCar();
 };
