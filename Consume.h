cb6bfa9d9b726b2e4684b248a262d86efb68ca5f
stewiegriffinaaa <3795447655@qq.com>
2025-06-26 22:53:32 +0800

    Update Consume.h

--- Consume.h
+++ Consume.h
@@ -5,14 +5,15 @@ using namespace std;
 class Consume
 {
 protected:
-  int num;
-  string name;
-  char _class;
-  string email;
-  Time _start;
+	string num;
+	string name;
+	char _class;
+	string email;
+	Time _start;
 public:
-  Consume();
-  Consume(int _num,string _name,char _Class,string _email,Time _Start);
-  friend ostream& operator<<(ostream& out, Consume& example);
+	Consume(string _num, string _name,char _Class, string _email, Time _Start);
+	friend ostream& operator<<(ostream& out, Consume& example);
+	friend istream& operator>>(istream& in, Consume& example);
 };
 ostream& operator<<(ostream& out, Consume& example);
+istream& operator>>(istream& in, Consume& example);
