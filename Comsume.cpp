414d337d47efdf6a635ae165106b460ea7661935
stewiegriffinaaa <3795447655@qq.com>
2025-06-26 22:56:22 +0800

    Update Comsume.cpp

--- Comsume.cpp
+++ Comsume.cpp
@@ -1,17 +1,18 @@
 #include"Consume.h"
 #include<string>
+#include"Time.h"
 using namespace std;
-Consume::Consume(){
-
+Consume::Consume(string _num, string _name, char _Class, string _email, Time _Start):_start(_Start) {
+	this->num = _num;
+	this->name = _name;
+	this->_class = _Class;
+	this->email = _email;
 }
-Consume::Consume(int _num,string _name,char _Class,string _email,Time _Start){
-  this->num = _num;
-  this->name = _name;
-  this->_class = _Class;
-  this->email = _email;
-  this->_start = _Start;
+ostream& operator<<(ostream& out, Consume& example) {
+	out << example.num << " " << example.name << " " << example._class << example.email << " " << example._start;
+	return out;
 }
-ostream& operator<<(ostream& out, Consume& example){
-  out << example.num << " " << example.name << " "  << example._class << example.email << " " << example._start;
-  return out;
-}
+istream& operator>>(istream& in, Consume& example){
+	in >> example.num >> example.name >> example._class >> example.email >> example._start;
+	return in;
+}//修改点六
