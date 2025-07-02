eb1a1c7b85e1e03e064efb523c72b6d3e047a8bf
stewiegriffinaaa <3795447655@qq.com>
2025-07-02 08:14:27 +0800

    Update ShopCar.cpp

--- ShopCar.cpp
+++ ShopCar.cpp
@@ -1,19 +1,33 @@
 #include"ShopCar.h"
 using namespace std;
 #include<sstream>
-ShopCar::ShopCar(string _num, string _name, char _class, string _email, Time _Start, int _ShopCar, int _ConsumeNum, Time _start, Time _end) : Consume(_num, _name, _class, _email, _Start)
-{
-    this->ShopCarNum = _ShopCar;
-    this->ConsumeNum = _ConsumeNum;
-    this->start = _start;
-    this->end = _end;
+#include<time.h>
+ShopCar::ShopCar() {            //必要性观察
+
 }
-ostringstream& ShopCar::show(ostringstream& out)
+ShopCar::ShopCar(Consume example)
+{
+    this->ShopCarNum = example.getNum();
+    this->ConsumeNum = example.getNum();
+    time_t now = time(NULL);
+    tm local_time = {};
+    localtime_s(&local_time, &now);
+    Time mid;
+    mid.year = local_time.tm_year + 1900;
+    mid.month = local_time.tm_mon + 1;
+    mid.day = local_time.tm_mday;
+    this->start = mid;
+    Time mid2;
+    mid2.year = mid.year + 20;
+    mid2.month = mid.month;
+    mid2.day = mid.day;
+    this->end = mid2;
+}
+ostream& operator<<(ostream& out,ShopCar& example)
 {
     int i = 0;
-    out << "购物车" << ShopCarNum << "号 " << "顾客" << ConsumeNum << "号 " << start << "至" << end << endl;
-    for (vector<Goods>::iterator it = num.begin(); it != num.end(); it++) {
-        out << *it << " " << "添入时间: " << addnum[i] << endl;
+    for (vector<Goods>::iterator it = example.num.begin(); it != example.num.end(); it++) {
+        out << example.ShopCarNum << " " << example.start << " " << example.end << " " << it->getId() << " " << example.addnum[i] << endl;
         i++;
     }
     return out;
@@ -29,6 +43,8 @@ void ShopCar::add(Goods sample)
     mid.day = local_time.tm_mday;
     addnum.push_back(mid);
     num.push_back(sample);
+ //   cout << "添加成功" << endl;  //wwwwwwwwwwwwwwwwww
+ //   cout << num.size() << endl;
 }
 void ShopCar::dec(Goods type, int numb) {
     for (vector<Goods>::iterator it = num.begin();it != num.end();it++) {
@@ -45,31 +61,40 @@ void ShopCar::dec(Goods type, int numb) {
     }
 }
 void ShopCar::CompleteCut(Goods type) {
-    for (vector<Goods>::iterator it = num.begin(); it != num.end(); it++) {
-        if ((*it).getId() == type.getId()) {
-            int mid = it - num.begin();          //补充彻底删除函数，主导系统删除任务（修改点二）
-            vector<Time>::iterator it1 = addnum.begin() + mid;
-            addnum.erase(it1);
+    // 使用迭代器遍历商品列表
+    for (auto it = num.begin(); it != num.end(); ) {
+        if (it->getId() == type.getId()) {
+            // 计算当前元素在vector中的位置
+            size_t index = it - num.begin();
+
+            // 删除对应的时间记录
+            addnum.erase(addnum.begin() + index);
+
+            // 删除当前商品，并更新迭代器
             it = num.erase(it);
+        }
+        else {
+            // 如果没有删除元素，正常推进迭代器
+            ++it;
         }
     }
 }
-int ShopCar::getShopCarNum() {
+string& ShopCar::getShopCarNum() {
     return ShopCarNum;
 }
-int ShopCar::getConsumeNum() {
+string& ShopCar::getConsumeNum() {
     return ConsumeNum;
 }
-Time ShopCar::getStart() {
+Time& ShopCar::getStart() {
     return start;
 }
-Time ShopCar::getEnd() {
+Time& ShopCar::getEnd() {
     return end;
 }
-vector<Goods> ShopCar::getNum() {
+vector<Goods>& ShopCar::getNum() {
     return num;
 }
-vector<Time> ShopCar::getAddnum() {
+vector<Time>& ShopCar::getAddnum() {
     return addnum;
 }
 ShopCar::~ShopCar() {}
