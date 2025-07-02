3fc3e89fdd478fb5cd867a245e994d7ee74651fc
stewiegriffinaaa <3795447655@qq.com>
2025-07-02 08:16:19 +0800

    Update Comsume.cpp

--- Comsume.cpp
+++ Comsume.cpp
@@ -2,14 +2,18 @@
 #include<string>
 #include"Time.h"
 using namespace std;
+Consume::Consume(){}
 Consume::Consume(string _num, string _name, char _Class, string _email, Time _Start):_start(_Start) {
 	this->num = _num;
 	this->name = _name;
 	this->_class = _Class;
 	this->email = _email;
 }
+string Consume::getNum() {
+	return num;
+}
 ostream& operator<<(ostream& out, Consume& example) {
-	out << example.num << " " << example.name << " " << example._class << example.email << " " << example._start;
+	out << example.num << " " << example.name << " " << example._class << " " << example.email << " " << example._start;
 	return out;
 }
 istream& operator>>(istream& in, Consume& example){
