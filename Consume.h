79ac0242cd1360920ac052bbcfdfd0b84e9c2e3e
stewiegriffinaaa <3795447655@qq.com>
2025-07-02 08:15:45 +0800

    Update Consume.h

--- Consume.h
+++ Consume.h
@@ -11,7 +11,9 @@ protected:
 	string email;
 	Time _start;
 public:
+	Consume();
 	Consume(string _num, string _name,char _Class, string _email, Time _Start);
+	string getNum();
 	friend ostream& operator<<(ostream& out, Consume& example);
 	friend istream& operator>>(istream& in, Consume& example);
 };
